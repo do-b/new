156a63fe3d06723cd3f00cb39e5dd5de8f33d7b0
do-b <rangwalaabdullah53@outlook.com>
2021-09-22 18:02:30 +0530

    beautify code

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@
 #include "WebSocketsServer.h"
 #include "ArduinoJson.h"
 
-#define DEBUG 1
+#define DEBUG 0
 
 #if DEBUG == 1
-#define debugstart(x) Serial.begin(9600)
+#define debugstart(x) Serial.begin(x)
 #define debug(x) Serial.print(x)
 #define debugln(x) Serial.println(x)
 #else
@@ -30,119 +30,98 @@ int txt = 0;
 char webpage[] PROGMEM = R"rawliteral(
 <!DOCTYPE html>
 <html>
-
-<head>
+  <head>
     <title>Ultrasonic</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" type="image/png" href="D:\Esp\favicon.png" />
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-
     <script>
-        var connection = new WebSocket('ws://' + location.hostname + ':81/');
+      var connection = new WebSocket('ws://' + location.hostname + ':81/');
+      var button_1_status = 0;
+      var dis_data1 = 0;
+      var dis_data2 = 0;
+      var text_data = 0;
+      var string1 = "tank is string1";
+      var string2 = "tank is string2";
 
-        var button_1_status = 0;
-        var dis_data1 = 0;
-        var dis_data2 = 0;
-        var text_data = 0;
-        var string1 = "tank is string1";
-        var string2 = "tank is string2";
-
-
-        function update_log() {
-            console.log("client requsted to update");
-            var full_data = '{"refresh" :"1"}';
-            connection.send(full_data);
+      function update_log() {
+        console.log("client requsted to update");
+        var full_data = '{"refresh":"1"}';
+        connection.send(full_data);
+      }
+      connection.onmessage = function(event) {
+        var full_data = event.data;
+        console.log(full_data);
+        var data = JSON.parse(full_data);
+        console.log(data);
+        dis_data1 = data.temp;
+        dis_data2 = data.cemp;
+        text_data = data.text;
+        document.getElementById("dis_meter1").value = dis_data1;
+        document.getElementById("dis_value1").innerHTML = dis_data1;
+        document.getElementById("dis_meter2").value = dis_data2;
+        document.getElementById("dis_value2").innerHTML = dis_data2;
+        if (text_data == 1) {
+          document.getElementById("text_data_from").innerHTML = string1;
+        } else {
+          document.getElementById("text_data_from").innerHTML = string2;
         }
-
-        connection.onmessage = function (event) {
-            var full_data = event.data;
-            console.log(full_data);
-            var data = JSON.parse(full_data);
-            console.log(data);
-            dis_data1 = data.temp;
-            dis_data2 = data.cemp;
-            text_data = data.text;
-
-            document.getElementById("dis_meter1").value = dis_data1;
-            document.getElementById("dis_value1").innerHTML = dis_data1;
-            document.getElementById("dis_meter2").value = dis_data2;
-            document.getElementById("dis_value2").innerHTML = dis_data2;
-
-            if (text_data == 1) {
-                document.getElementById("text_data_from").innerHTML = string1;
-            }
-            else {
-                document.getElementById("text_data_from").innerHTML = string2;
-            }
-
-        }
-
-
-
+      }
     </script>
-
-
     <style>
-        body {
-            animation: fadeInAnimation ease 3s;
-            animation-iteration-count: 1;
-            animation-fill-mode: forwards;
+      body {
+        animation: fadeInAnimation ease 3s;
+        animation-iteration-count: 1;
+        animation-fill-mode: forwards;
+      }
+
+      @keyframes fadeInAnimation {
+        0% {
+          opacity: 0;
         }
 
-        @keyframes fadeInAnimation {
-            0% {
-                opacity: 0;
-            }
-
-            100% {
-                opacity: 1;
-            }
+        100% {
+          opacity: 1;
         }
+      }
 
+      new meter,
+      meter,
+      h3 {
+        display: inline
+      }
 
-        new meter,
-        meter,
-        h3 {
-            display: inline
-        }
-
-        title h3,
-        h2 {
-            display: inline;
-        }
+      title h3,
+      h2 {
+        display: inline;
+      }
     </style>
-</head>
-
-<body>
+  </head>
+  <body>
     <center>
-        <h1>Ultasonic sensor reading!</h1>
-
-        <p>update the readings of sensor by clicking below button</p>
-        <button onclick="update_log()"> Update </button>
-
-        <div style="text-align: center;">
-            <div class="title">
-                <h3>Tank_vol--------</h3>
-                <h3>Tank_Vo2</h3>
-            </div>
-            <div class="new">
-                <meter value="100" min="0" max="30" id="dis_meter1"> </meter>
-                <h3>--------</h3>
-                <meter value="100" min="0" max="30" id="dis_meter2"> </meter>
-            </div>
-            <h3 id="dis_value1"> 15</h3>
-            <h3>cm</h3>
-            <h3>-----------</h3>
-            <h3 id="dis_value2"> 30</h3>
-            <h3>cm</h3>
+      <h1>Ultasonic sensor reading!</h1>
+      <p>update the readings of sensor by clicking below button</p>
+      <button onclick="update_log()"> Update </button>
+      <div style="text-align: center;">
+        <div class="title">
+          <h3>Tank_vol--------</h3>
+          <h3>Tank_Vo2</h3>
         </div>
-        <h3 id="text_data_from"></h3>
+        <div class="new">
+          <meter value="100" min="0" max="30" id="dis_meter1"></meter>
+          <h3>--------</h3>
+          <meter value="100" min="0" max="30" id="dis_meter2"></meter>
+        </div>
+        <h3 id="dis_value1"> 15</h3>
+        <h3>cm</h3>
+        <h3>-----------</h3>
+        <h3 id="dis_value2"> 30</h3>
+        <h3>cm</h3>
+      </div>
+      <h3 id="text_data_from"></h3>
     </center>
-</body>
-
+  </body>
 </html>
-
 )rawliteral"; // wrtite "   R"rawliteral( __________ )rawliteral"   " for not convert c+ string
 
 AsyncWebServer server(80); //http protocol prt 80 define, for diffrent protocol https://www.meridianoutpost.com/resources/articles/well-known-tcpip-ports.php
@@ -225,19 +204,19 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
   switch (type)
   {
   case WStype_DISCONNECTED:
-    Serial.printf("[%u] Disconnected!\n", num);
+    //Serial.printf("[%u] Disconnected!\n", num);
     break;
   case WStype_CONNECTED:
   {
     IPAddress ip = websockets.remoteIP(num);
-    Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
+    //Serial.printf("[%u] Connected from %d.%d.%d.%d url: %s\n", num, ip[0], ip[1], ip[2], ip[3], payload);
 
     // send message to client
     websockets.sendTXT(num, "Connected from server");
   }
   break;
   case WStype_TEXT:
-    Serial.printf("[%u] get Text: %s\n", num, payload);
+    //Serial.printf("[%u] get Text: %s\n", num, payload);
     String message = String((char *)(payload));
     debugln(message);
 
@@ -262,7 +241,7 @@ void webSocketEvent(uint8_t num, WStype_t type, uint8_t *payload, size_t length)
       digitalWrite(LED, LOW);
       delay(500);
     }
-    break;
+    break;   
   }
 }
 
